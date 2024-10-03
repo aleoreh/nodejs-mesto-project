79e56e646992590558f633bea2a0b6d2f62a4e43
Aleksey Orekhov <aleoreh@gmail.com>
2024-10-03 20:14:46 +0300

    Добавляет urlencoded и json middleware

--- src/app.ts
+++ src/app.ts
@@ -12,6 +12,9 @@ const app = express();
 
 mongoose.connect("mongodb://localhost:27017/mestodb");
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 app.use("/users", users);
 
 app.listen(3000);

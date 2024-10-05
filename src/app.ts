69fac8f1af2a99326df70cbe4f27f8c34e102617
Aleksey Orekhov <aleoreh@gmail.com>
2024-10-05 13:16:51 +0300

    Улучшает запуск приложения

--- src/app.ts
+++ src/app.ts
@@ -11,31 +11,41 @@ import usersRouter from "./routes/users";
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
 
-const { FAKE_USER_ID = "123456" } = process.env;
+const {
+  DATABASE_PATH = "mongodb://127.0.0.1:27017/mestodb",
+  FAKE_USER_ID = "123456",
+} = process.env;
 
-const app = express();
+function run() {
+  const app = express();
 
-mongoose.connect("mongodb://localhost:27017/mestodb");
+  app.use(express.urlencoded({ extended: false }));
+  app.use(express.json());
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+  app.use((req: Request, res: Response, next: NextFunction) => {
+    res.locals.user = {
+      _id: FAKE_USER_ID,
+    };
 
-app.use((req: Request, res: Response, next: NextFunction) => {
-  res.locals.user = {
-    _id: FAKE_USER_ID,
-  };
+    next();
+  });
 
-  next();
-});
+  app.use("/users", usersRouter);
+  app.use("/cards", cardsRouter);
 
-app.use("/users", usersRouter);
-app.use("/cards", cardsRouter);
+  app.use(appErrorHandler);
+  app.use(errorLogger);
+  app.use(finalErrorHandler);
 
-app.use(appErrorHandler);
-app.use(errorLogger);
-app.use(finalErrorHandler);
+  app.listen(3000, () => {
+    console.log("Listening on port 3000");
+    console.log(`User: ${FAKE_USER_ID}`);
+  });
+}
 
-app.listen(3000, () => {
-  console.log("Listening on port 3000");
-  console.log(`User: ${FAKE_USER_ID}`);
-});
+mongoose
+  .connect(DATABASE_PATH)
+  .then(run)
+  .catch(() => {
+    console.log(`Не удалось подключиться к базе данных ${DATABASE_PATH}`);
+  });
